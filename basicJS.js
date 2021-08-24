d495fd0f6246396e62859c095e996ef5831b4693
ernsch777 <82405659+ernsch777@users.noreply.github.com>
2021-08-23 19:19:34 -0700

    Update basicJS.js

--- basicJS.js
+++ basicJS.js
@@ -23,4 +23,52 @@ function onlyBabies(x) {
 }
 
 let babyDogNames = pets.filter(onlyDogs).filter(onlyBabies).map(nameOnly)
-console.log(babyDogNames)
+
+let myName = "Brad"
+
+function amazingFunction() {
+    let myName = "Ernie"
+    if (2 + 2 == 4) {
+        console.log(myName)
+    }
+    console.log("inside the function")
+}
+console.log(myName)
+amazingFunction()
+
+let john = {
+    firstName: "John",
+    lastName: "Doe",
+    driveCar() {
+        function imAFunctionNotAMethod() {
+            console.log(this)
+        }
+        imAFunctionNotAMethod()
+        console.log(this.firstName + " " + this.lastName + " is driving a car")
+    }
+}
+
+john.driveCar()
+
+function breathe() {
+    console.log(this.firstName + " " + this.lastName + " just inhaled and exhaled.")
+}
+breathe.call(john)
+
+
+document.addEventListener("click", () => alert("thank you for clicking"))
+
+let myNumbers = [10, 500, 2000]
+
+let doubleNumbers = myNumbers.map(x => x * 2)
+console.log(doubleNumbers)
+
+// function cool (){
+//   console.log("This is super cool.")
+// }
+
+let cool = function () {
+    console.log("hey")
+}
+
+cool()
